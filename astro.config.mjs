da8bb60f728be794884f9152a0d9ff9bb63cc168
Dhruthick Gowda <dhruthick1215.m@gmail.com>
2024-03-26 11:38:04 -0700

    github pages deployment setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://<username>.github.io',
-  base: '/website'
+  base: '/website',
   integrations: [mdx(), sitemap(), tailwind()]
 });
